4b5d72a24a7abf84a1d20fe79c0a36ae585c20b3
ShotGunAlphaYT <92694923+ShotGunAlphaYT@users.noreply.github.com>
2022-05-06 20:37:42 -0500

    6

---
+++ Users/Administator/user_settings.js
@@ -0,0 +1,8 @@
+var create_files = true;
+var edit_files = true;
+var delete_files = true;
+var save_data = true;
+
+function main () {
+    let all = (create_files, edit_files, delete_files, save_data);
+}
